a9dd82fd81e687bc8810fe5522938150b49b2058
Syed Muhammed Bilal <intergo878@gmail.com>
2020-05-11 00:22:29 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 
+/* Functional Component */
+function DisplayNames(props){
+  
+  return(
+    <div>
+        <ul>
+          {
+            props.names.map((n, index) => 
+            <li key={index}>{n}</li>)
+          }
+        </ul>
+    </div>
+  )
 
-class Friends extends Component {
+}
+
+
+
+/* Class Component */
+class ListItems extends Component {
+
   render() {
-    const names = ["Bilal", "Ahmed", "Abdulrehman"]
-    
     return(
       <div>
         <ul>
           {
-            names.map((n, index) => <li key={index}>{n}</li>)
+            this.props.cityNames.map((n, index) => 
+            <li key={index}>{n}</li>)
           }
         </ul>
+        <button onClick={this.props.plzChangeTheState} > Press to increment </button>
       </div>
     )
   }
 }
 
+
+
 class App extends Component {
+
+  state = {
+    cityNames: ["Kashmir", "Karachi", "Islamabad"],
+    address: "Axiom"
+  }
+
+  changeTheState = () => {this.setState(
+    () => ({
+      cityNames: ["Bilal", "Ahmed", "Abdulrehman"]
+    })
+  )}
+
   render() {
+    /*const names = ["Bilal", "Ahmed", "Abdulrehman"]*/
     return(
       <div>
-        <Friends />
-        <Friends />
+        <ListItems cityNames={this.state.cityNames} plzChangeTheState={this.changeTheState} />
+
       </div>
     )
   }
